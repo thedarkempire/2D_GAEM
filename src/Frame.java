f6a23355c82891cdcea118ab777d6b51476ba5bb
thedarkempire <thedarkempire10@gmail.com>
2023-01-29 22:51:59 +0530

    updated version

--- src/Frame.java
+++ src/Frame.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Frame {
     static Weapons weapons;
@@ -28,12 +30,22 @@ public class Frame {
         b1.setText("Equipment");
         b1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                JFrame f1 = new JFrame();
-                f1.setSize(600, 600);//size of new frame
-                f1.setVisible(true);
-                f1.setLayout(null);
-                f1.setResizable(false);
-                f1.setLocationRelativeTo(null);
+                JFrame f2 = new JFrame();
+                f2.setSize(600, 600);//size of new frame
+                f2.setVisible(true);
+                f2.setLayout(null);
+                f2.setResizable(false);
+                f2.setLocationRelativeTo(null);
+                f2.addWindowListener(new WindowAdapter() {
+                    public void windowOpened(WindowEvent windowEvent) {
+                b1.setEnabled(false);
+                    }
+                    });
+                f2.addWindowListener(new WindowAdapter() {
+                    public void windowClosed(WindowEvent e) {
+                        b1.setEnabled(true);
+                    }
+                });
 
                 JButton eqp1 = new JButton("armour1");
                 eqp1.setBounds(100, 50, 400, 100);//size of button and coordinates
@@ -42,7 +54,7 @@ public class Frame {
                     public void actionPerformed(ActionEvent e) {
                         equip = Game.arm.get(1);
                         System.out.println("Armour Equipped!");
-                        f1.dispose();//closes the current frame without closing the main frame
+                        f2.dispose();//closes the current frame without closing the main frame
                     }
                 });
 
@@ -53,7 +65,7 @@ public class Frame {
                     public void actionPerformed(ActionEvent e) {
                         equip = Game.arm.get(2);
                         System.out.println("Armour Equipped!");
-                        f1.dispose();//closes the current frame without closing the main frame
+                        f2.dispose();//closes the current frame without closing the main frame
                     }
                 });
 
@@ -64,13 +76,13 @@ public class Frame {
                     public void actionPerformed(ActionEvent e) {
                         equip = Game.arm.get(3);
                         System.out.println("Armour Equipped!");
-                        f1.dispose();//closes the current frame without closing the main frame
+                        f2.dispose();//closes the current frame without closing the main frame
                     }
                 });
-                f1.add(eqp1);
-                f1.add(eqp2);
-                f1.add(eqp3);
-                f1.setDefaultCloseOperation(f1.DISPOSE_ON_CLOSE);//closes the current frame without closing the main frame
+                f2.add(eqp1);
+                f2.add(eqp2);
+                f2.add(eqp3);
+                f2.setDefaultCloseOperation(f2.DISPOSE_ON_CLOSE);//closes the current frame without closing the main frame
             }
         });
         j.add(b1);//adds button to main frame
@@ -89,6 +101,16 @@ public class Frame {
                 f1.setLayout(null);
                 f1.setResizable(false);
                 f1.setLocationRelativeTo(null);
+                f1.addWindowListener(new WindowAdapter() {
+                    public void windowOpened(WindowEvent windowEvent) {
+                        b2.setEnabled(false);
+                    }
+                });
+                f1.addWindowListener(new WindowAdapter() {
+                    public void windowClosed(WindowEvent e) {
+                        b2.setEnabled(true);
+                    }
+                });
 
                 JButton a1 = new JButton("Bow");
                 a1.setBounds(100, 50, 400, 100);//size of button and coordinates
@@ -122,6 +144,7 @@ public class Frame {
                         f1.dispose();//closes the current frame without closing the main frame
                     }
                 });
+
                 f1.add(a1);
                 f1.add(a2);
                 f1.add(a3);
@@ -139,6 +162,16 @@ public class Frame {
                 j2.setSize(1280, 720);
                 j2.setResizable(false);
                 j2.setLocationRelativeTo(null);
+                j2.addWindowListener(new WindowAdapter() {
+                    public void windowOpened(WindowEvent windowEvent){
+                        b3.setEnabled(false);
+                    }
+                });
+                j2.addWindowListener(new WindowAdapter() {
+                    public void windowClosed(WindowEvent e) {
+                        b3.setEnabled(true);
+                    }
+                });
 
                 j2.setLocationRelativeTo(null);//centers the frame to the screen
                 j2.setVisible(true);
@@ -168,24 +201,26 @@ public class Frame {
                 t1.setBounds(764, 486, 150, 25);
                 t1.setForeground(Color.WHITE);
                 t1.setFont(new Font("Comic Sans", Font.PLAIN, 20));
-                /*JButton restart=new JButton("restart");
-                restart.setBounds(800,100,100,50);
+                JLabel t2=new JLabel();
+                t2.setText("");
+                JButton restart=new JButton("exit");
+                restart.setBounds(1150,50,100,50);
                 restart.addActionListener(new ActionListener() {
-                                              public void actionPerformed(ActionEvent e) {
-                                                  Game.entity.reset();
-                                                  Entity.Player.reset();
-                                              }
-                                          });
-                                          j2.add(restart);*/
+                public void actionPerformed(ActionEvent e) {
+                    Entity.Player.reset();
+                    Game.entity.reset();
+                j2.dispose();
+                }
+                });
+                j2.add(restart);
 
                 j2.add(t);
                 j2.add(t1);
                 j2.add(l);
                 j2.add(l1);
                 j2.setDefaultCloseOperation(j2.DISPOSE_ON_CLOSE);//closes the current frame without closing the main frame
-                if (j2.isActive()) {
-                    b3.setEnabled(false);
-                }
+                t.setText("Your hp:" + Math.max(0, Entity.Player.hp));
+                t1.setText("Enemy hp:" + Math.max(0, Game.entity.hp));
                     JButton atk = new JButton("Attack");
                     atk.setBounds(1100, 150, 100, 50);
                     atk.addActionListener(new ActionListener() {
@@ -195,12 +230,16 @@ public class Frame {
                                 Game.entity.hp -= (Game.damage(Entity.Player, weapons));
                                 System.out.println("You have " + Entity.Player.hp + " left");
                                 t.setText("Your hp:" + Entity.Player.hp);
-                                t1.setText("Enemy hp:" + Game.entity.hp);
-                                System.out.println("Your opponent has " + Game.entity.hp + " left");
+                                t1.setText("Enemy hp:" + Math.max(0, Game.entity.hp));
+                                System.out.println("Your opponent has " +Math.max(0, Game.entity.hp)+ " left");
                                 if (Game.entity.hp <= 0) {
                                     System.out.println("You defeated Your opponent!!! " + Game.entity.name);
+                                    Entity.Player.reset();
+                                    Game.entity.reset();
                                 } else if(Entity.Player.hp <= 0) {
                                     System.out.println("You have been defeated by " + Game.entity.name);
+                                    Entity.Player.reset();
+                                    Game.entity.reset();
                                 }
                             }
                         }
@@ -220,8 +259,12 @@ public class Frame {
                                 System.out.println("Your opponent has " + Game.entity.hp + " left");
                                 if (Game.entity.hp <= 0) {
                                     System.out.println("You defeated Your opponent!!! " + Game.entity.name);
+                                    Entity.Player.reset();
+                                    Game.entity.reset();
                                 } else if(Entity.Player.hp <= 0) {
                                     System.out.println("You have been defeated by " + Game.entity.name);
+                                    Entity.Player.reset();
+                                    Game.entity.reset();
                                 }
                             }
                         }
@@ -241,8 +284,12 @@ public class Frame {
                                 System.out.println("Your opponent has " + Game.entity.hp + " left");
                                 if (Game.entity.hp <= 0) {
                                     System.out.println("You defeated Your opponent!!! " + Game.entity.name);
+                                    Entity.Player.reset();
+                                    Game.entity.reset();
                                 } else if(Entity.Player.hp <= 0) {
                                     System.out.println("You have been defeated by " + Game.entity.name);
+                                    Entity.Player.reset();
+                                    Game.entity.reset();
                                 }
                             }
                         }
